3d097794e55dded8a75f5f4625e87c3cf81989d6
lujanfernaud <lujanfernaud@gmail.com>
2017-09-26 16:57:53 +0100

    Improve validations for Passenger model

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -4,5 +4,9 @@ class Passenger < ApplicationRecord
 
   has_many :flights, through: :bookings
 
-  validates :name, :email, presence: true
+  validates :name, presence: true, length: { in: 3..64 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
 end
