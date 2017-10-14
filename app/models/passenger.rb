29d996a4ab110cc1ad3bbdcf095ea636850d3f63
lujanfernaud <lujanfernaud@gmail.com>
2017-10-14 08:41:48 +0100

    Hide email in booking view
    Hide email until bookings can only be seen by its owners.

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -13,4 +13,10 @@ class Passenger < ApplicationRecord
   def full_name
     first_name + " " + last_name
   end
+
+  def email_hidden
+    email_parts = email.match(/(^\w\w+\.?\w+)(\@\w+\.\w{3})/)
+    user, domain = email_parts[1], email_parts[2]
+    user.gsub(/\w/, "*") + domain.gsub(/\w/, "*")
+  end
 end
